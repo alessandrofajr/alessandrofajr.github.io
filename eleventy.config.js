29b318496b238f1b777e5c308d618f86ac6b8215
Alessandro Feitosa Jr <alessandrofajunior@gmail.com>
2024-08-07 17:52:19 -0300

    Adicionando opção para exibir imagens

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/favicon.svg");
 
+  eleventyConfig.addPassthroughCopy("src/img");
+
   eleventyConfig.addPassthroughCopy("src/CNAME");
 
   eleventyConfig.addCollection("blog", function(collection) {
